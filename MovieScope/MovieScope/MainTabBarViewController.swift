9b80e3cc0c5cb3dfd3172279750328f9cc533668
Developer <indira@MacBook-Pro-Indira-2.local>
2023-10-18 10:42:34 +0600

    initial version

---
+++ MovieScope/MovieScope/MainTabBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MovieScope
+//
+//  Created by Developer on 18.10.2023.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemYellow
+        
+    }
+
+
+}
+
